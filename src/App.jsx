54f539bb40ed6936418e997e3f6c98a201eee6fb
raghvendra <zumiburiburi@gmail.com>
2025-09-24 18:20:56 +0530

    link the pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Signin from './pages/Signin'
 import About from './pages/About'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/sign-in' element={<Signin />}/>
