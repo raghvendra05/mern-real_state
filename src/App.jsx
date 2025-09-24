66c91ca741ef374068e008991c7c799db369a9d9
raghvendra <zumiburiburi@gmail.com>
2025-09-24 13:16:32 +0530

    routes setingup using browser router and make pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Signin from './pages/Signin'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div className='text-blue-800'>App</div>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/sign-in' element={<Signin />}/>
+          <Route path='/sign-up' element={<SignUp />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/profile' element={<Profile />}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
