754e6a8c9d11473f92b45c9a89b61e3df4a6add7
raghvendra <zumiburiburi@gmail.com>
2025-09-25 15:01:55 +0530

    env and mongoose

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+    .then((client) => {
+        console.log("mongo running");
+    }).catch(error => {
+        console.log("mongo error");
+
+    })
 
 const app = express()
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server is alive at 3000")
 })
