13660264fb8a67b6ad334c53ae3045226e4c65f3
raghvendra <zumiburiburi@gmail.com>
2025-09-25 17:01:37 +0530

    auth hasing using 'bcrypt' tested post api

--- api/index.js
+++ api/index.js
@@ -3,22 +3,26 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO)
-    .then((client) => {
-        console.log("mongo running");
-    }).catch(error => {
-        console.log("mongo error");
 
-    })
+mongoose.connect(process.env.MONGO)
+.then((client) => {
+    console.log("mongo running");
+}).catch(error => {
+    console.log("mongo error");
+    
+})
 
 const app = express()
 
+app.use(express.json())//without tis we will not be able to send psot requset in bruno
 
 app.listen(3000, () => {
     console.log("server is alive at 3000")
 })
 
 app.use("/api/user",userRouter)
+app.use("/api/auth",authRouter)
