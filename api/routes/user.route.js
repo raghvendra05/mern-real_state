8e815ca0042a0983dac07d3af7436e4b22e987b7
raghvendra <zumiburiburi@gmail.com>
2025-09-26 14:58:27 +0530

    complete signup page functonallity

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 
-import { test } from '../controllers.js/user.controller.js'
+import { test } from '../controllers/user.controller.js'
 
 
 router.get("/test",test)
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy: {
+      // this means all the req with '/api' add this 'http://localhost:3000' part before it 
+      '/api': {
+        target: 'http://localhost:3000',
+        secure:false,
+      }
+    }
+  },
   plugins: [react(), tailwindcss()],
 })
